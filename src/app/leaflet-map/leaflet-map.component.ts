785a4f96927620eb10f9a5ff7d411da724b7402d
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-06 05:53:55 +0800

    Implement Two-way binding on zoom lat and long changes

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ElementRef, Input, Output, EventEmitter } from '@angular/core';
 import { LeafletTileProviderService } from '../leaflet-tile-provider.service';
 import * as L from 'leaflet';
 
@@ -9,15 +9,22 @@ import * as L from 'leaflet';
 })
 export class LeafletMapComponent implements OnInit {
   @Input() lat:number;
+  @Output() latChange: EventEmitter<number> = new EventEmitter<number>();
+
   @Input() lng:number;
+  @Output() lngChange: EventEmitter<number> = new EventEmitter<number>();
+
   @Input() zoom:number;
+  @Output() zoomChange: EventEmitter<number> = new EventEmitter<number>();
+
+  public map: any;
 
   constructor(private element: ElementRef, private tileProvider: LeafletTileProviderService) {}
 
   ngOnInit() {
     let el = this.element.nativeElement.querySelector('.map');
 
-    // set default view parameters
+    // set default view parameters to show whole Philippines
     let lat = 13;
     let lng = 122;
     let zoom = 6;
@@ -38,15 +45,29 @@ export class LeafletMapComponent implements OnInit {
     }
 
     // create the map instance
-    let map:any = L.map(el, {
+    this.map = L.map(el, {
       zoomControl: false
     });
 
     // position the map
-    map.setView([lat, lng], zoom);
+    this.map.setView([lat, lng], zoom);
 
     // add the one of the basemap to the map
-    this.tileProvider.baseMaps['OpenStreetMap'].addTo(map);
+    this.tileProvider.baseMaps['OpenStreetMap'].addTo(this.map);
+
+    // emit changes to lat and lng coordinates
+    this.map.on('moveend', (e) => {
+      let center = this.map.getCenter();
+
+      this.latChange.emit(center.lat);
+
+      this.lngChange.emit(center.lng);
+    });
+
+    // emit changes to zoom
+    this.map.on('zoomend', (e) => {
+      this.zoomChange.emit(this.map.getZoom());
+    });
   }
 
 }
