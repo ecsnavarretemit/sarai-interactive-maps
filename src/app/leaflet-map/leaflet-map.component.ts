552c08d95e426083e41dcb1295332f8c56bbff97
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-05 22:50:28 +0800

    Get lat, long and zoom from the component attribute

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 import { LeafletTileProviderService } from '../leaflet-tile-provider.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-map',
@@ -8,21 +8,47 @@ import { LeafletTileProviderService } from '../leaflet-tile-provider.service';
   styleUrls: ['./leaflet-map.component.sass']
 })
 export class LeafletMapComponent implements OnInit {
+  @Input() lat:number;
+  @Input() lng:number;
+  @Input() zoom:number;
 
   constructor(private element: ElementRef, private tileProvider: LeafletTileProviderService) {}
 
   ngOnInit() {
     let el = this.element.nativeElement.querySelector('.map');
 
-    let map:any = L
-      .map(el, {
-        zoomControl: false
-      })
-      .setView([13, 122], 6)
-      ;
+    // set default view parameters
+    let lat = 13;
+    let lng = 122;
+    let zoom = 6;
+
+    // override the lat variable if lat is provided via the lat attribute
+    if (typeof this.lat !== 'undefined') {
+      lat = this.lat;
+    }
+
+    // override the lng variable if lng is provided via the lng attribute
+    if (typeof this.lng !== 'undefined') {
+      lng = this.lng;
+    }
+
+    // override the zoom variable if zoom is provided via the zoom attribute
+    if (typeof this.zoom !== 'undefined') {
+      zoom = this.zoom;
+    }
+
+    // create the map instance
+    let map:any = L.map(el, {
+      zoomControl: false
+    });
+
+    // position the map
+    map.setView([lat, lng], zoom);
 
     // add the one of the basemap to the map
-    this.tileProvider.baseMaps['OpenStreetMap'].addTo( map );
+    this.tileProvider.baseMaps['OpenStreetMap'].addTo(map);
   }
 
 }
+
+
