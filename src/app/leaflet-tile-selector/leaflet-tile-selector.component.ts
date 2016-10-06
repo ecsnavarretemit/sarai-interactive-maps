ed7358bb6d5fe38c1bb7539100c768421ad408af
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-06 11:16:50 +0800

    Add subscriber to reflect the currently selected map tile provider

--- src/app/leaflet-tile-selector/leaflet-tile-selector.component.ts
+++ src/app/leaflet-tile-selector/leaflet-tile-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { LeafletTileProviderService } from '../leaflet-tile-provider.service';
 
 @Component({
@@ -6,16 +7,36 @@ import { LeafletTileProviderService } from '../leaflet-tile-provider.service';
   templateUrl: './leaflet-tile-selector.component.html',
   styleUrls: ['./leaflet-tile-selector.component.sass']
 })
-export class LeafletTileSelectorComponent implements OnInit {
+export class LeafletTileSelectorComponent implements OnInit, AfterViewInit {
   @Input() map:any;
   @Output() tileChange: EventEmitter<string> = new EventEmitter<string>();
 
   public tileKeys: any;
 
-  constructor(private tileProvider: LeafletTileProviderService) {}
+  constructor(
+    public store: Store<any>,
+    private element: ElementRef,
+    private tileProvider: LeafletTileProviderService
+  ) {}
 
   ngOnInit() {
     this.tileKeys = Object.keys(this.tileProvider.baseMaps);
+  }
+
+  ngAfterViewInit() {
+    let selectEl = this.element.nativeElement.querySelector('#map-tile-selector');
+
+    // TODO: unsuscribe once it fired once already
+    this.store
+      .select('map')
+      .subscribe((state: any) => {
+        if (state.tileProvider !== null && selectEl !== null && selectEl.value !== state.tileProvider) {
+          console.log('trigger');
+
+          selectEl.value = state.tileProvider;
+        }
+      })
+      ;
   }
 
   onTileChange(event) {
