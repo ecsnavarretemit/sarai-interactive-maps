7ee055700c1c88ab43b46468424201b88099b906
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-06 18:31:54 +0800

    Reorganize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Store, StoreModule } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { TooltipModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+import { mapReducer } from './map.state';
+import { LeafletTileProviderService } from './leaflet-tile-provider.service';
 
 import { AppComponent } from './app.component';
 import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
-
-import { LeafletTileProviderService } from './leaflet-tile-provider.service';
 import { LeafletTileSelectorComponent } from './leaflet-tile-selector/leaflet-tile-selector.component';
-
-import { mapReducer } from './map.state';
 import { LeafletGeocoderComponent } from './leaflet-geocoder/leaflet-geocoder.component';
 import { LeafletZoomComponent } from './leaflet-zoom/leaflet-zoom.component';
 import { LeafletMeasureComponent } from './leaflet-measure/leaflet-measure.component';
@@ -27,7 +25,6 @@ import { LeafletMeasureComponent } from './leaflet-measure/leaflet-measure.compo
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     TooltipModule,
     StoreModule.provideStore({
