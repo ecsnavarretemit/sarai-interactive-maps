2494dbd1a77cf2f84a332958a000e858d5d5dc42
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-07 13:55:15 +0800

    Fix map attributions

--- src/app/leaflet-tile-provider.service.ts
+++ src/app/leaflet-tile-provider.service.ts
@@ -11,32 +11,34 @@ export class LeafletTileProviderService {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors'
       }),
       OpenCycle: L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://www.opencyclemap.org/">OpenCylcle</a> contributors'
+        attribution: 'Map data &copy; <a href="http://www.opencyclemap.org/" target="_blank">OpenCylcle</a> contributors'
       }),
       Mapnik: L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://mapnik.org/">Mapnik</a> contributors'
+        attribution: 'Map data &copy; <a href="http://mapnik.org/" target="_blank">Mapnik</a> contributors'
       })
     };
 
     // Google Maps Tiles
     this.baseMaps['Google Satellite'] = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: 'Map data &copy; <a href="https://www.google.com/" target="_blank">Google</a>'
     });
 
     this.baseMaps['Google Street'] = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        attribution: 'Map data &copy; <a href="https://www.google.com/" target="_blank">Google</a>'
     });
 
     // Carto Tiles
     this.baseMaps['CartoDB Dark'] = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
-          &copy; <a href="http://cartodb.com/attributions">CartoDB</a>`
+        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
+          &copy; <a href="http://cartodb.com/attributions" target="_blank">CartoDB</a>`
     });
 
     this.baseMaps['CartoDB Light'] =  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors,
-          &copy; <a href="http://cartodb.com/attributions">CartoDB</a>`
-    })
+        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
+          &copy; <a href="http://cartodb.com/attributions" target="_blank">CartoDB</a>`
+    });
   }
 
 }
