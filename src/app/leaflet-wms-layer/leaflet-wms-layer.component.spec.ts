f6a70fdf6e6b51d9a814fd7e32193b3e1e09877a
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-10 09:28:23 +0800

    Initial working example of wms layer component

---
+++ src/app/leaflet-wms-layer/leaflet-wms-layer.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+/*!
+ * Leaflet WMS Layer Component Test
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { TestBed, async } from '@angular/core/testing';
+import { LeafletWmsLayerComponent } from './leaflet-wms-layer.component';
+
+describe('Component: LeafletWmsLayer', () => {
+  it('should create an instance', () => {
+    let component = new LeafletWmsLayerComponent();
+    expect(component).toBeTruthy();
+  });
+});
+
+
---
+++ src/app/leaflet-wms-layer/leaflet-wms-layer.component.ts
@@ -0,0 +1,53 @@
+/*!
+ * Leaflet WMS Layer Component
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Map, WMS } from 'leaflet';
+
+@Component({
+  selector: 'app-leaflet-wms-layer',
+  templateUrl: './leaflet-wms-layer.component.html',
+  styleUrls: ['./leaflet-wms-layer.component.sass']
+})
+export class LeafletWmsLayerComponent implements OnInit, AfterViewInit {
+  public layer: WMS;
+
+  @Input() map: Map;
+
+  constructor() { }
+
+  ngOnInit() {
+    if (typeof this.map === 'undefined') {
+      throw new Error('Please provide the map first via the map attribute.');
+    }
+
+    let workspace = 'sarai-latest';
+    let url = `http://202.92.144.40:8080/geoserver/${workspace}/wms?tiled=true`;
+
+    let leafletApi = (L as any);
+
+    // create the WMS tile layer
+    this.layer = leafletApi.tileLayer.wms(url, {
+      layers: workspace + ':rice_merged',
+      format: 'image/png',
+      transparent: true,
+      maxZoom: 10,
+      crs: leafletApi.CRS.EPSG900913,
+      attribution: `Crop data &copy; 2016
+        <a href="http://www.pcaarrd.dost.gov.ph/" target="_blank">PCAARRD</a> and
+        <a href="http://uplb.edu.ph/" target="_blank">University of the Philippines Los Banos</a>`
+    });
+  }
+
+  ngAfterViewInit() {
+    // add to the map when the view is finally initialized
+    this.layer.addTo( this.map );
+  }
+
+}
+
+
