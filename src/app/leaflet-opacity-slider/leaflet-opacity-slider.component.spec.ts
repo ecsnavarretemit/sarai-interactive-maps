03aca4b3c603db7447f105f1de3bae366e84ffff
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-11 10:54:37 +0800

    Create layer opacity control

---
+++ src/app/leaflet-opacity-slider/leaflet-opacity-slider.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+/*!
+ * Leaflet Opacity Slider Component Test
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { TestBed, async } from '@angular/core/testing';
+import { LeafletOpacitySliderComponent } from './leaflet-opacity-slider.component';
+
+describe('Component: LeafletOpacitySlider', () => {
+  it('should create an instance', () => {
+    let component = new LeafletOpacitySliderComponent();
+    expect(component).toBeTruthy();
+  });
+});
+
+
