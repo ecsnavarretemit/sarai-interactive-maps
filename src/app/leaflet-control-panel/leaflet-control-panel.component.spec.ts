df5e96119ea6a634c29d1a637af53c196b41885d
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-11 08:06:11 +0800

    Create control panel component

---
+++ src/app/leaflet-control-panel/leaflet-control-panel.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+/*!
+ * Leaflet Control Panel
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { TestBed, async } from '@angular/core/testing';
+import { LeafletControlPanelComponent } from './leaflet-control-panel.component';
+
+describe('Component: LeafletControlPanel', () => {
+  it('should create an instance', () => {
+    let component = new LeafletControlPanelComponent();
+    expect(component).toBeTruthy();
+  });
+});
+
+
