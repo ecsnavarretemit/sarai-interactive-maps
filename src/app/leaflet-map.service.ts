6b8de1c5ee7f164d2191bd0d2a122559c6f0568f
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-12 10:46:43 +0800

    Improve layer add and remove logic

--- src/app/leaflet-map.service.ts
+++ src/app/leaflet-map.service.ts
@@ -7,12 +7,13 @@
 
 import { Injectable } from '@angular/core';
 import { Map, MapOptions, WMS, WMSOptions } from 'leaflet';
+import * as _ from 'lodash';
 
 @Injectable()
 export class LeafletMapService {
   private _map: Promise<Map>;
   private _mapResolver: (value?: Map) => void;
-  private _wmsLayers: Array<WMS> = [];
+  private _wmsLayers: any = {};
 
   constructor() {
     this._map = new Promise<Map>((resolve: () => void) => {
@@ -30,16 +31,20 @@ export class LeafletMapService {
 
   getMap(): Promise<Map> { return this._map; }
 
-  addWMSLayer(url: string, options: WMSOptions): Promise<WMS> {
+  addWMSLayer(id: string, url: string, options: WMSOptions): Promise<WMS> {
     return this.getMap()
       .then((map: Map) => {
+        if (_.has(this._wmsLayers, id)) {
+          throw new Error('ID already exists. Provide another ID for this layer');
+        }
+
         let layer = (L as any).tileLayer.wms(url, options);
 
         // add the layer to the map
         map.addLayer(layer);
 
         // save the reference of the layer to the layers propery
-        this._wmsLayers.push(layer);
+        this._wmsLayers[id] = layer;
 
         // return the layer
         return layer;
@@ -47,21 +52,34 @@ export class LeafletMapService {
       ;
   }
 
-  addWMSLayers(items: Array<any>): Promise<WMS> {
+  addWMSLayers(url: string, items: Array<{id: string, options: WMSOptions}>): Promise<Array<WMS>> {
     return this.getMap()
       .then((map: Map) => {
-        let layers = items.map((item: any) => {
-          let layer = (L as any).tileLayer.wms(item.url, item.options);
+        let layers = _.chain(items)
+          .filter((item) => {
+            let result = _.has(this._wmsLayers, item.id);
 
-          // the created layer to the map
-          map.addLayer(layer);
+            if (result) {
+              throw new Error(`ID ${item.id} exists. please provide a new ID for this layer.`);
+            }
 
-          // return the layer
-          return layer;
-        });
+            return !result;
+          })
+          .map((item) => {
+            let layer = (L as any).tileLayer.wms(url, item.options);
 
-        // append to the existing layers
-        this._wmsLayers.concat(layers);
+            // the created layer to the map
+            map.addLayer(layer);
+
+            // store the key and its layer to the key-value store
+            this._wmsLayers[item.id] = layer;
+
+            return layer;
+          })
+          .value()
+          ;
+
+        console.log(layers);
 
         return layers;
       })
@@ -72,17 +90,28 @@ export class LeafletMapService {
     return this.getMap()
       .then((map: Map) => {
         // remove any existing layers
-        this._wmsLayers.forEach((value: WMS) => {
+        _.each(this._wmsLayers, (value: WMS, id: string) => {
           map.removeLayer(value);
         });
 
         // set the layers to empty array
-        this._wmsLayers = [];
+        this._wmsLayers = {};
       })
       ;
   }
 
-  getWMSLayers(): Promise<Array<WMS>> {
+  removeWMSLAyer(id: string): Promise<void> {
+    return this.getMap()
+      .then((map: Map) => {
+        map.removeLayer(this._wmsLayers[id]);
+
+        // remove the property from the object store.
+        delete this._wmsLayers[id];
+      })
+      ;
+  }
+
+  getWMSLayers(): Promise<any> {
     return this.getMap()
       .then((map: Map) => {
         return this._wmsLayers;
