7e14d028dd3354541bba0ab2da8b13e0a28ec0be
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-12 13:49:09 +0800

    Create home component

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+/*!
+ * Home Component Test
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HomeComponent } from './home.component';
+
+describe('Component: Home', () => {
+  it('should create an instance', () => {
+    let component = new HomeComponent();
+    expect(component).toBeTruthy();
+  });
+});
+
+
