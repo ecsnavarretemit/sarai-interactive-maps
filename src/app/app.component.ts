47fe2f690dd161d1bcaaa11415264144aec92252
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-19 09:40:47 +0800

    Remove crop and redirect logic from the AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@
  */
 
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,48 +14,8 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   public layersOpacity = 0.6;
-  public cropData: Array<any> = [];
 
-  constructor(public router: Router) {
-    this.cropData = [
-      {
-        name: 'Rice',
-        slug: 'rice'
-      }, {
-        name: 'Corn',
-        slug: 'corn',
-        subcrops: [
-          {name: 'Corn Dry', slug: 'corn-dry'},
-          {name: 'Corn Wet', slug: 'corn-wet'}
-        ]
-      }, {
-        name: 'Banana',
-        slug: 'banana'
-      }, {
-        name: 'Coconut',
-        slug: 'coconut'
-      }, {
-        name: 'Coffee',
-        slug: 'coffee',
-        subcrops: [
-          {name: 'Coffee Arabica', slug: 'coffee-arabica'},
-          {name: 'Coffee Robusta', slug: 'coffee-robusta'}
-        ]
-      }, {
-        name: 'Cacao',
-        slug: 'cacao'
-      }
-    ];
-  }
-
-  suitabilityRedirect(event, crop: string, containsChild = true) {
-    if (containsChild) {
-      return;
-    }
-
-    // redirect to the URL
-    this.router.navigateByUrl(`/suitability-maps/${crop}`);
-  }
+  constructor() {}
 
 }
 
