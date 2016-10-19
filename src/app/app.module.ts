4507bffb49dced727d1ab8cab534f7a865ce601e
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-19 20:30:40 +0800

    Import angular 2 forms module for binding models to form elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { StoreModule } from '@ngrx/store';
 import { TooltipModule, AccordionModule } from 'ng2-bootstrap/ng2-bootstrap';
@@ -59,6 +60,7 @@ import { RainfallMapPanelComponent } from './rainfall-map-panel/rainfall-map-pan
   imports: [
     BrowserModule,
     SaraiNg2RoutingModule,
+    FormsModule,
     HttpModule,
     TooltipModule,
     AccordionModule,
