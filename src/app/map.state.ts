b654c09fb18f76d3110ad120adad2397922a6edc
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-20 09:18:51 +0800

    Add the ability to add multiple layers to map state store

--- src/app/map.state.ts
+++ src/app/map.state.ts
@@ -6,7 +6,7 @@
  */
 
 import { ActionReducer, Action } from '@ngrx/store';
-import { assign, map, filter } from 'lodash';
+import { assign, map, filter, isArray } from 'lodash';
 
 export const mapReducer: ActionReducer<any> = (state: any = [], action: Action) => {
   switch (action.type) {
@@ -24,9 +24,7 @@ export const mapReducer: ActionReducer<any> = (state: any = [], action: Action) 
     case 'UPDATE_LAYER':
       return map(state, (layer: any) => {
         if (layer.id === action.payload.id) {
-          let obj: any = {
-            data: action.payload.data
-          };
+          let obj: any = {};
 
           if (typeof action.payload.id !== 'undefined') {
             obj.id = action.payload.id;
@@ -39,6 +37,9 @@ export const mapReducer: ActionReducer<any> = (state: any = [], action: Action) 
           if (typeof action.payload.url !== 'undefined') {
             obj.url = action.payload.url;
           }
+
+          // prevent ovewriting data
+          obj.data = assign({}, layer.data, action.payload.data);
 
           return assign({}, layer, obj);
         }
@@ -51,12 +52,29 @@ export const mapReducer: ActionReducer<any> = (state: any = [], action: Action) 
         return layer.id !== action.payload;
       });
 
+    case 'ADD_LAYERS':
+      if (!isArray(action.payload)) {
+        return state;
+      }
+
+      let newLayers = map(action.payload, (data: any) => {
+        return assign({}, {
+          id: data.id,
+          zoom: data.zoom,
+          url: data.url,
+          data: data.data
+        });
+      });
+
+      return [
+        ...state,
+        ...newLayers
+      ];
+
     case 'UPDATE_LAYERS_BY_ZOOM':
       return map(state, (layer: any) => {
         if (layer.zoom === action.payload.zoom) {
-          let obj: any = {
-            data: action.payload.data
-          };
+          let obj: any = {};
 
           if (typeof action.payload.id !== 'undefined') {
             obj.id = action.payload.id;
@@ -69,6 +87,9 @@ export const mapReducer: ActionReducer<any> = (state: any = [], action: Action) 
           if (typeof action.payload.url !== 'undefined') {
             obj.url = action.payload.url;
           }
+
+          // prevent ovewriting data
+          obj.data = assign({}, layer.data, action.payload.data);
 
           return assign({}, layer, obj);
         }
