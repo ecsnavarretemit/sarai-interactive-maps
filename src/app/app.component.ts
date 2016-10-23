e093cfe500572bd32d5f10a9b8d3ceffcdff12a9
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-23 16:27:55 +0800

    Add buttons for social and interface controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@
  * Licensed under MIT
  */
 
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { WindowService } from './window.service';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public layersOpacity = 0.6;
 
-  constructor() {}
+  constructor(@Inject(WindowService) private _window: Window) { }
+
+  openNewWindow(event: Event, windowName: string, windowFeatures = '') {
+    // prevent default behavior
+    event.preventDefault();
+
+    // mark as any to prevent errors
+    let href = (event.target as any).href;
+
+    if (typeof href === 'undefined') {
+      return;
+    }
+
+    // open new window
+    this._window.open(href, windowName, windowFeatures);
+  }
 
 }
 
