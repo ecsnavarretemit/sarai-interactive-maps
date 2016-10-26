cd548c72221173e26e36c7c46731e8f344408431
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-26 22:56:38 +0800

    Persist user language preference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@
  */
 
 import { Component, Inject } from '@angular/core';
+import { CookieService } from 'angular2-cookie/core';
 import { WindowService } from './window.service';
 import { TranslateService } from 'ng2-translate';
 
@@ -17,13 +18,28 @@ import { TranslateService } from 'ng2-translate';
 export class AppComponent {
   public layersOpacity = 0.6;
   private _currentLang = 'en';
+  private _cookieLangKey = 'app_lang';
 
-  constructor(@Inject(WindowService) private _window: Window, private _translate: TranslateService) {
+  constructor(
+    @Inject(WindowService) private _window: Window,
+    private _translate: TranslateService,
+    private _cookieService: CookieService
+  ) {
+    // retreive language preference from the cookie
+    let lang = this._cookieService.get(this._cookieLangKey);
+
+    if (typeof lang === 'undefined') {
+      this._cookieService.put(this._cookieLangKey, this._currentLang);
+
+      // use the default language
+      lang = this._currentLang;
+    }
+
     // this language will be used as a fallback when a translation isn't found in the current language
-    this._translate.setDefaultLang(this._currentLang);
+    this._translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    this._translate.use(this._currentLang);
+    this._translate.use(lang);
   }
 
   openNewWindow(event: Event, windowName: string, windowFeatures = '') {
@@ -50,6 +66,9 @@ export class AppComponent {
     } else {
       this._currentLang = 'en';
     }
+
+    // store the new language preference
+    this._cookieService.put(this._cookieLangKey, this._currentLang);
 
     // change the current language
     this._translate.use(this._currentLang);
