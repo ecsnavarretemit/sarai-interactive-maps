6baabad188352d40fd3f3247c60bd7db7a84e222
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-30 07:11:16 +0800

    Show and hide opacity control when map has no layers overlayed

--- src/app/leaflet-opacity-slider/leaflet-opacity-slider.component.ts
+++ src/app/leaflet-opacity-slider/leaflet-opacity-slider.component.ts
@@ -5,8 +5,12 @@
  * Licensed under MIT
  */
 
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Input } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
 import { WMS } from 'leaflet';
+import { LayerState } from '../store';
 import { LeafletMapService } from '../leaflet-map.service';
 
 @Component({
@@ -14,13 +18,35 @@ import { LeafletMapService } from '../leaflet-map.service';
   templateUrl: './leaflet-opacity-slider.component.html',
   styleUrls: ['./leaflet-opacity-slider.component.sass']
 })
-export class LeafletOpacitySliderComponent implements OnInit {
+export class LeafletOpacitySliderComponent implements OnInit, OnDestroy {
+  public hasLayers: boolean;
+  private _mapLayers: Observable<LayerState>;
+  private _mapLayersSubscription: Subscription;
+
   @Input() title: string = 'Layer Overlay Opacity';
   @Input() opacity: number = 1;
   @ViewChild('controlwrapper') controlWrapper: ElementRef;
   @ViewChild('range') range: ElementRef;
 
-  constructor(private _mapService: LeafletMapService) {}
+  constructor(
+    private _mapLayersStore: Store<any>,
+    private _mapService: LeafletMapService
+  ) {
+    // add a flag to show or hide the control
+    this.hasLayers = false;
+
+    // get the map state store from the store
+    this._mapLayers = this._mapLayersStore.select('mapLayers');
+
+    // check if our store has layers and fire subscribe every 300ms
+    this._mapLayersSubscription = this._mapLayers
+      .debounceTime(300)
+      .subscribe((layerState: LayerState) => {
+        // set the flag to the value of the condition
+        this.hasLayers = (layerState.ids.length > 0);
+      })
+      ;
+  }
 
   ngOnInit() {
     // reflect the default value
@@ -43,6 +69,11 @@ export class LeafletOpacitySliderComponent implements OnInit {
 
   adjustOpacity(event) {
     this.setOpacity(this.range.nativeElement.value);
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to the store observable
+    this._mapLayersSubscription.unsubscribe();
   }
 
 }
