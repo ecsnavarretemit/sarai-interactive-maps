d394b018feb68e00ea3bcf1bc0a90928c22da22c
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-29 16:32:11 +0800

    Create reducer/store for suitability levels

---
+++ src/app/store/index.ts
@@ -0,0 +1,11 @@
+/*!
+ * Store Importer
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+export * from './map-layers/map-layers.reducer';
+export * from './suitability-levels/suitability-levels.reducer';
+
+
---
+++ src/app/store/suitability-levels/suitability-levels.reducer.spec.ts
@@ -0,0 +1,21 @@
+/* tslint:disable:no-unused-variable */
+
+/*!
+ * Suitability Levels Reducer Test
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { async, inject } from '@angular/core/testing';
+import { SuitabilityLevelsReducer } from './suitability-levels.reducer';
+
+describe('MapState', () => {
+
+  it('should create an instance', () => {
+    expect(SuitabilityLevelsReducer).toBeTruthy();
+  });
+
+});
+
+
