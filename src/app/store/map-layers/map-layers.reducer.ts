8cb49e4368a215f5b17be30f3fd0bbaa0d4367ab
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-29 14:10:01 +0800

    Initial map layers reducer refactoring

--- src/app/store/map-layers/map-layers.reducer.ts
+++ src/app/store/map-layers/map-layers.reducer.ts
@@ -6,149 +6,219 @@
  */
 
 import { ActionReducer, Action } from '@ngrx/store';
-import { assign, map, filter, isArray, has } from 'lodash';
+import { assign, map, filter, isArray, has, omit, size, reduce, forEach } from 'lodash';
+import { WMSOptions } from 'leaflet';
 
-export const MapLayersReducer: ActionReducer<any> = (state: any = [], action: Action) => {
+export interface Layer {
+  id: string;
+  url: string;
+  type: string; // suitability-map-detailed, suitability-map-simplified, or other category
+  data: {
+    wmsOptions: WMSOptions
+  };
+}
+
+export interface LayerState {
+  ids: Array<string>;
+  layers: {
+    [id: string]: Layer
+  };
+}
+
+const initialState: LayerState = {
+  ids: [],
+  layers: {}
+};
+
+export const MapLayersReducer: ActionReducer<LayerState> = (state: LayerState = initialState, action: Action) => {
   switch (action.type) {
-    case 'ADD_LAYER':
-      return [
-        ...state,
-        assign({}, {
-          id: action.payload.id,
-          zoom: action.payload.zoom,
-          url: action.payload.url,
-          data: action.payload.data
+    case 'ADD_LAYER': {
+      const layer = action.payload;
+
+      // if already existing return the current state
+      if (state.ids.indexOf(layer.id) > -1) {
+        return state;
+      }
+
+      // return the new state
+      return {
+        ids: [
+          ...state.ids,
+          layer.id
+        ],
+        layers: assign({}, state.layers, {
+          [layer.id]: layer
         })
-      ];
+      };
+    }
 
-    case 'UPDATE_LAYER':
-      return map(state, (layer: any) => {
-        if (layer.id === action.payload.id) {
-          let obj: any = {};
+    case 'REMOVE_LAYER': {
+      // create new object based on the layers without the layer that matches the payload value
+      const remainingLayers: any = omit(state.layers, action.payload);
 
-          if (typeof action.payload.id !== 'undefined') {
-            obj.id = action.payload.id;
-          }
+      // return the initial state when
+      if (size(remainingLayers) === 0) {
+        return initialState;
+      }
 
-          if (typeof action.payload.zoom !== 'undefined') {
-            obj.zoom = action.payload.zoom;
-          }
+      // extract the remaining ids from the layers
+      const remainingIds: any = map(remainingLayers, 'id');
 
-          if (typeof action.payload.url !== 'undefined') {
-            obj.url = action.payload.url;
-          }
+      // return the new state
+      return {
+        ids: remainingIds,
+        layers: remainingLayers
+      };
+    }
 
-          // prevent ovewriting data
-          obj.data = assign({}, layer.data, action.payload.data);
-
-          return assign({}, layer, obj);
-        }
-
-        return layer;
-      });
-
-    case 'REMOVE_LAYER':
-      return filter(state, (layer: any) => {
-        return layer.id !== action.payload;
-      });
-
-    case 'ADD_LAYERS':
+    case 'ADD_LAYERS': {
       if (!isArray(action.payload)) {
         return state;
       }
 
-      let newLayers = map(action.payload, (data: any) => {
-        return assign({}, {
-          id: data.id,
-          zoom: data.zoom,
-          url: data.url,
-          data: data.data
+      const layers = action.payload;
+
+      // remove the existing layers from the object
+      const newLayers = filter(layers, (layer: Layer) => {
+        return !has(state.layers, layer.id);
+      });
+
+      // extract the ids from the layers
+      const newLayerIds: any = map(newLayers, 'id');
+
+      // assemble the new object to match the data structure of the layers attribute of the state
+      const newLayerLayers: any = reduce(newLayers, (layers: { [id: string]: Layer }, layer: Layer) => {
+        return assign(layers, {
+          [layer.id]: layer
+        });
+      }, {});
+
+      // return the new state
+      return {
+        ids: [
+          ...state.ids,
+          ...newLayerIds
+        ],
+
+        layers: assign({}, state.layers, newLayerLayers)
+      };
+    }
+
+    case 'REMOVE_ALL_LAYERS':
+      return initialState;
+
+    case 'ADD_CQL_FILTER_BY_ID': {
+      // copy the layers to prevent any state modification
+      const currentLayers = assign({}, state.layers);
+
+      // if id does not exist, return the current state
+      if (!has(currentLayers, action.payload.id)) {
+        return state;
+      }
+
+      // save current wmsOptions of the layer
+      let wmsOptions = currentLayers[action.payload.id].data.wmsOptions;
+
+      // add the cql_filter
+      currentLayers[action.payload.id].data.wmsOptions = assign({}, wmsOptions, {
+        cql_filter: action.payload.cql_filter
+      });
+
+      return {
+        ids: [...state.ids],
+        layers: assign({}, state.layers, currentLayers)
+      };
+    }
+
+    case 'REMOVE_CQL_FILTER_BY_ID': {
+      // copy the layers to prevent any state modification
+      const currentLayers = assign({}, state.layers);
+
+      // if id does not exist, return the current state
+      if (!has(currentLayers, action.payload)) {
+        return state;
+      }
+
+      // remove the cql_filter from the options
+      currentLayers[action.payload].data.wmsOptions = (omit(currentLayers[action.payload].data.wmsOptions, 'cql_filter') as any);
+
+      return {
+        ids: [...state.ids],
+        layers: assign({}, state.layers, currentLayers)
+      };
+    }
+
+    case 'ADD_CQL_FILTER_BY_TYPE': {
+      // copy the layers to prevent any state modification
+      const currentLayers = assign({}, state.layers);
+
+      // get all layers that matches the type payload
+      let layersToModify = filter(currentLayers, (layer: Layer) => {
+        return layer.type === action.payload.type;
+      });
+
+      // return immediately if no layer's matched in the filter
+      if (size(layersToModify) === 0) {
+        return state;
+      }
+
+      forEach(layersToModify, (layer: Layer, key: string) => {
+        // save current wmsOptions of the layer
+        let wmsOptions = layer.data.wmsOptions;
+
+        // add the cql_filter
+        layersToModify[key].data.wmsOptions = assign({}, wmsOptions, {
+          cql_filter: action.payload.cql_filter
         });
       });
 
-      return [
-        ...state,
-        ...newLayers
-      ];
+      // assemble the new object to match the data structure of the layers attribute of the state
+      const modifiedLayers: any = reduce(layersToModify, (layers: { [id: string]: Layer }, layer: Layer) => {
+        return assign(layers, {
+          [layer.id]: layer
+        });
+      }, {});
 
-    case 'UPDATE_LAYERS_BY_ZOOM':
-      return map(state, (layer: any) => {
-        if (layer.zoom === action.payload.zoom) {
-          let obj: any = {};
+      return {
+        ids: [...state.ids],
+        layers: assign({}, state.layers, modifiedLayers)
+      };
+    }
 
-          if (typeof action.payload.id !== 'undefined') {
-            obj.id = action.payload.id;
-          }
+    case 'REMOVE_CQL_FILTER_BY_TYPE': {
+      // copy the layers to prevent any state modification
+      const currentLayers = assign({}, state.layers);
 
-          if (typeof action.payload.zoom !== 'undefined') {
-            obj.zoom = action.payload.zoom;
-          }
-
-          if (typeof action.payload.url !== 'undefined') {
-            obj.url = action.payload.url;
-          }
-
-          // prevent ovewriting data
-          obj.data = assign({}, layer.data, action.payload.data);
-
-          return assign({}, layer, obj);
-        }
-
-        return layer;
+      // get all layers that matches the type payload
+      let layersToModify = filter(currentLayers, (layer: Layer) => {
+        return layer.type === action.payload;
       });
 
-    // TODO: add cql_filter based on the layer id
-    case 'ADD_CQL_FILTER_BY_ZOOM':
-      return map(state, (layer: any) => {
-        if (layer.zoom === action.payload.zoom) {
-          let obj: any = {};
+      // return immediately if no layer's matched in the filter
+      if (size(layersToModify) === 0) {
+        return state;
+      }
 
-          // prevent ovewriting data
-          obj.data = assign({}, layer.data);
+      forEach(layersToModify, (layer: Layer, key: string) => {
+        // save current wmsOptions of the layer
+        let wmsOptions = layer.data.wmsOptions;
 
-          // add cql_filter option when wmsOptions property exists
-          if (has(obj, 'data.wmsOptions')) {
-            obj.data.wmsOptions = assign({}, obj.data.wmsOptions, {
-              cql_filter: action.payload.cql_filter
-            });
-          }
-
-          // store the gridcodes on the data
-          if (has(action.payload, 'gridcodes')) {
-            obj.data.gridcodes = action.payload.gridcodes;
-          }
-
-          return assign({}, layer, obj);
-        }
-
-        return layer;
+        // remove the cql_filter
+        layersToModify[key].data.wmsOptions = (omit(wmsOptions, 'cql_filter') as any);
       });
 
-    // TODO: remove cql_filter based on the layer id
-    case 'REMOVE_CQL_FILTER_BY_ZOOM':
-      return map(state, (layer: any) => {
-        if (layer.zoom === action.payload.zoom) {
-          // remove the cql_filter option
-          if (has(layer, 'data.wmsOptions.cql_filter')) {
-            delete layer.data.wmsOptions.cql_filter;
-          }
+      // assemble the new object to match the data structure of the layers attribute of the state
+      const modifiedLayers: any = reduce(layersToModify, (layers: { [id: string]: Layer }, layer: Layer) => {
+        return assign(layers, {
+          [layer.id]: layer
+        });
+      }, {});
 
-          // remove the gridcodes
-          if (has(layer, 'data.gridcodes')) {
-            delete layer.data.gridcodes;
-          }
-        }
-
-        return layer;
-      });
-
-    case 'REMOVE_LAYERS_BY_ZOOM':
-      return filter(state, (layer: any) => {
-        return layer.zoom !== action.payload;
-      });
-
-    case 'REMOVE_ALL_LAYERS':
-      return [];
+      return {
+        ids: [...state.ids],
+        layers: assign({}, state.layers, modifiedLayers)
+      };
+    }
 
     default:
       return state;
