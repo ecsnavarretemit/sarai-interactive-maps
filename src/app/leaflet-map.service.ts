cf5ed97b5ac236309412afff64f89724027e3c83
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-30 19:11:31 +0800

    Add the ability to add and remove tile layers

--- src/app/leaflet-map.service.ts
+++ src/app/leaflet-map.service.ts
@@ -6,14 +6,19 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Map, MapOptions, WMS, WMSOptions } from 'leaflet';
+import { Map, MapOptions, TileLayer, TileLayerOptions, WMS, WMSOptions } from 'leaflet';
 import * as _ from 'lodash';
+
+interface LayerCollection {
+  [id: string]: TileLayer | WMS;
+}
 
 @Injectable()
 export class LeafletMapService {
   private _map: Promise<Map>;
   private _mapResolver: (value?: Map) => void;
-  private _wmsLayers: any = {};
+  private _wmsLayers: LayerCollection = {};
+  private _tileLayers: LayerCollection = {};
 
   constructor() {
     this._map = new Promise<Map>((resolve: () => void) => {
@@ -31,9 +36,79 @@ export class LeafletMapService {
 
   getMap(): Promise<Map> { return this._map; }
 
+  addTileLayer(id: string, layer: TileLayer): Promise<void> {
+    return this.getMap()
+      .then((map: Map) => {
+        if (_.has(this._tileLayers, id)) {
+          throw new Error('ID already exists. Provide another ID for this layer');
+        }
+
+        // add the layer to the map
+        map.addLayer(layer);
+
+        // save the reference of the layer to the layers propery
+        this._tileLayers[id] = layer;
+      })
+      ;
+  }
+
+  addNewTileLayer(id: string, url: string, options: TileLayerOptions): Promise<TileLayer> {
+    return this.getMap()
+      .then((map: Map) => {
+        if (_.has(this._tileLayers, id)) {
+          throw new Error('ID already exists. Provide another ID for this layer');
+        }
+
+        let layer = (L as any).tileLayer(url, options);
+
+        // add the layer to the map
+        map.addLayer(layer);
+
+        // save the reference of the layer to the layers propery
+        this._tileLayers[id] = layer;
+
+        // return the layer
+        return layer;
+      })
+      ;
+  }
+
+  removeTileLayer(id: string): Promise<void> {
+    return this.getMap()
+      .then((map: Map) => {
+        if (!_.has(this._tileLayers, id)) {
+          throw new Error('ID does not exist. Cannot remove WMS layer.');
+        }
+
+        map.removeLayer(this._tileLayers[id]);
+
+        // remove the property from the object store.
+        delete this._tileLayers[id];
+      })
+      ;
+  }
+
+  clearTileLayers(): Promise<void> {
+    return this.getMap()
+      .then((map: Map) => {
+        // remove any existing layers
+        _.each(this._tileLayers, (value: TileLayer, id: string) => {
+          map.removeLayer(value);
+        });
+
+        // set the layers to empty array
+        this._tileLayers = {};
+      })
+      ;
+  }
+
   addWMSLayer(id: string, layer: WMS): Promise<void> {
     return this.getMap()
       .then((map: Map) => {
+        if (_.has(this._wmsLayers, id)) {
+          throw new Error('ID already exists. Provide another ID for this layer');
+        }
+
         // add the layer to the map
         map.addLayer(layer);
 
@@ -90,8 +165,6 @@ export class LeafletMapService {
           })
           .value()
           ;
-
-        console.log(layers);
 
         return layers;
       })
