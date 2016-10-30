3a10294080c7eb638132f3022cbc47bfc54d4263
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-30 08:49:19 +0800

    Fix tile provider service

--- src/app/leaflet-tile-provider.service.ts
+++ src/app/leaflet-tile-provider.service.ts
@@ -18,7 +18,7 @@ export class LeafletTileProviderService {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors'
       }),
       OpenCycle: L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://www.opencyclemap.org/" target="_blank">OpenCylcle</a> contributors'
+        attribution: 'Map data &copy; <a href="http://www.opencyclemap.org/" target="_blank">OpenCycle</a> contributors'
       }),
       Mapnik: L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://mapnik.org/" target="_blank">Mapnik</a> contributors'
@@ -38,12 +38,12 @@ export class LeafletTileProviderService {
 
     // Carto Tiles
     this.baseMaps['CartoDB Dark'] = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
+        attribution: `Map data &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
           &copy; <a href="http://cartodb.com/attributions" target="_blank">CartoDB</a>`
     });
 
     this.baseMaps['CartoDB Light'] =  L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-        attribution: `&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
+        attribution: `Map data &copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors,
           &copy; <a href="http://cartodb.com/attributions" target="_blank">CartoDB</a>`
     });
   }
