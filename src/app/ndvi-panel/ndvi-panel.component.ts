c656234633fc35c283b1fd542b669a93688b23ea
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-10-30 17:30:11 +0800

    Implement basic form form filtering NDVI data

--- src/app/ndvi-panel/ndvi-panel.component.ts
+++ src/app/ndvi-panel/ndvi-panel.component.ts
@@ -6,6 +6,8 @@
  */
 
 import { Component, OnInit, Output, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-ndvi-panel',
@@ -13,12 +15,35 @@ import { Component, OnInit, Output, ViewChild, ElementRef, EventEmitter } from '
   styleUrls: ['./ndvi-panel.component.sass']
 })
 export class NdviPanelComponent implements OnInit {
+  public filterForm: FormGroup;
+  public startDate: FormControl;
+  public scanRange: FormControl;
+
   @Output() panelIconClick: EventEmitter<Event> = new EventEmitter();
   @ViewChild('controlwrapper') controlWrapper: ElementRef;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {
+    this.startDate = new FormControl('', [
+      Validators.required,
+      CustomValidators.dateISO
+    ]);
+
+    this.scanRange = new FormControl('', [
+      Validators.required,
+      CustomValidators.number
+    ]);
+
+    this.filterForm = this._formBuilder.group({
+      startDate: this.startDate,
+      scanRange: this.scanRange
+    });
+  }
 
   ngOnInit() { }
+
+  processRequest() {
+    console.log(this.filterForm.value);
+  }
 
   onPanelIconClick(event) {
     this.panelIconClick.emit(event);
