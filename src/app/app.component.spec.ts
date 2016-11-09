dd8ec1d0f386a045f06f1029ef4c1d5db6236549
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-11-10 05:05:28 +0800

    Fix app component unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,20 +11,16 @@ import { TestBed, async } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule, Http } from '@angular/http';
-import { TooltipModule, AccordionModule, ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { Http } from '@angular/http';
+import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { TranslateModule, TranslateLoader } from 'ng2-translate';
-import { MdCheckboxModule } from '@angular2-material/checkbox';
 import { SaraiNg2RoutingModule } from './app-routing.module';
 import { StoreModule } from './store';
-import { LeafletModule } from './leaflet';
 import { MapModule } from './map';
 
 import { CookieService } from 'angular2-cookie/services/cookies.service';
 import { AppLoggerService } from './app-logger.service';
-
 import { TranslationFactoryLoader } from './app-translation-factory.service';
-import { MapLayersReducer, SuitabilityLevelsReducer } from './store';
 
 import { AppComponent } from './app.component';
 
@@ -34,15 +30,8 @@ describe('App: SaraiNg2', () => {
     TestBed.configureTestingModule({
       imports: [
         SaraiNg2RoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        HttpModule,
-        TooltipModule,
-        AccordionModule,
         ModalModule,
-        MdCheckboxModule,
         StoreModule,
-        LeafletModule,
         MapModule,
         TranslateModule.forRoot({
           provide: TranslateLoader,
