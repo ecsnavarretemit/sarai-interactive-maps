4b820fb7d865b922452f785f62742277c44a60fb
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-11-10 10:23:42 +0800

    Make value reactive on value change of the Crop, Region and Provice select fields

--- src/app/map/download-image-form/download-image-form.component.ts
+++ src/app/map/download-image-form/download-image-form.component.ts
@@ -5,14 +5,18 @@
  * Licensed under MIT
  */
 
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { LocationsService } from '../locations.service';
 import { SuitabilityMapService } from '../suitability-map.service';
 import { Crop } from '../crop.interface';
 import { trim, parseInt, reduce, each } from 'lodash';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'app-download-image-form',
@@ -31,12 +35,16 @@ export class DownloadImageFormComponent implements OnInit, OnDestroy {
   public pdfUrl: string = '#';
   public pdfFilename: string | null = null;
   private _regionChangeSubscription: Subscription;
-  private _provinceChangeSubscription: Subscription;
+  private _combinedRejectSubscription: Subscription;
+  private _combinedResolveSubscription: Subscription;
+
+  @Output() processComplete: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(
+    private _http: Http,
     private _formBuilder: FormBuilder,
     private _locationsService: LocationsService,
-    private _suitabilityMapService: SuitabilityMapService
+    private _suitabilityMapService: SuitabilityMapService,
   ) {
     this.selectCrop = new FormControl('', [
       Validators.required
@@ -105,20 +113,31 @@ export class DownloadImageFormComponent implements OnInit, OnDestroy {
       })
       ;
 
-    this._provinceChangeSubscription = this.selectProvince.valueChanges
-      .subscribe((value: any) => {
-        let trimmed = trim(value);
+    this._combinedRejectSubscription = Observable
+      .combineLatest(this.selectCrop.valueChanges, this.selectRegion.valueChanges, this.selectProvince.valueChanges)
+      .filter((values: [string, string, string]) => {
+        return (values[0] === '' || values[1] === '' || values[2] === '');
+      })
+      .debounceTime(300)
+      .subscribe((values: [string, string, string]) => {
+        this.pdfUrl = '#';
+        this.pdfFilename = null;
+      })
+      ;
 
-        // reset the form element state when value is empty
-        if (trimmed === '' || this.selectCrop.value === '#') {
-          this.pdfUrl = '#';
-          this.pdfFilename = null;
-          return;
-        }
+    this._combinedResolveSubscription = Observable
+      .combineLatest(this.selectCrop.valueChanges, this.selectRegion.valueChanges, this.selectProvince.valueChanges)
+      .filter((values: [string, string, string]) => {
+        return (values[0] !== '' && values[1] !== '' && values[2] !== '');
+      })
+      .debounceTime(300)
+      .subscribe((values: [string, string, string]) => {
+        let crop = values[0];
+        let province = values[2];
 
         // reflect the new values to the url and file name
-        this.pdfFilename = `${this.selectCrop.value}-${value}.pdf`;
-        this.pdfUrl = `/assets/docs/crops/${this.selectCrop.value}/${value}.pdf`;
+        this.pdfFilename = `${crop}-${province}.pdf`;
+        this.pdfUrl = `/assets/docs/crops/${crop}/${province}.pdf`;
       })
       ;
   }
@@ -130,13 +149,29 @@ export class DownloadImageFormComponent implements OnInit, OnDestroy {
   }
 
   processRequest() {
-    let value = this.downloadForm.value;
-    console.log(value);
+    // check first if the resolved url exists by sending a HEAD request
+    // if it exists emit the actual filename. if not emit a false value for the filename
+    this._http
+      .head(this.pdfUrl)
+      .subscribe((res: Response) => {
+        this.processComplete.emit({
+          filename: this.pdfFilename,
+          url: this.pdfUrl
+        });
+      }, (res: Response) => {
+        this.processComplete.emit({
+          filename: false,
+          url: false
+        });
+      })
+      ;
   }
 
   ngOnDestroy() {
     // unsubscribe to the changes in subscription element
     this._regionChangeSubscription.unsubscribe();
+    this._combinedRejectSubscription.unsubscribe();
+    this._combinedResolveSubscription.unsubscribe();
   }
 
 }
