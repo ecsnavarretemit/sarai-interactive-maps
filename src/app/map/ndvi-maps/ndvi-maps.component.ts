76902f531b56a1186d42a8c8d0bf95458f5d7933
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-12-21 06:43:48 +0800

    Export http request logic to a tile layer service

--- src/app/map/ndvi-maps/ndvi-maps.component.ts
+++ src/app/map/ndvi-maps/ndvi-maps.component.ts
@@ -5,10 +5,9 @@
  * Licensed under MIT
  */
 
-import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Http, Response } from '@angular/http';
-import { MAP_CONFIG } from '../map.config';
+import { Http } from '@angular/http';
 import { Store } from '@ngrx/store';
 import { LeafletMapService } from '../../leaflet';
 import { TileLayerService } from '../tile-layer.service';
@@ -28,7 +27,6 @@ export class NdviMapsComponent implements OnInit, OnDestroy {
   private _layerId: string;
 
   constructor(
-    @Inject(MAP_CONFIG) private _config: any,
     private _mapService: LeafletMapService,
     private _tileLayerService: TileLayerService,
     private _logger: AppLoggerService,
@@ -59,66 +57,40 @@ export class NdviMapsComponent implements OnInit, OnDestroy {
       type: 'REMOVE_ALL_LAYERS'
     });
 
-    // throw error if endpoint does not exist
-    if (typeof this._config.ndvi_maps.eeApiEndpoint === 'undefined' || this._config.ndvi_maps.eeApiEndpoint === '') {
-      throw new Error('API Endpoint for NDVI Layers not specified');
-    }
-
-    let endpoint = this._config.ndvi_maps.eeApiEndpoint;
-    let method = 'post';
-    let args = [endpoint, {
-      date: startDate,
-      range: scanRange
-    }];
-
-    if (this._config.ndvi_maps.eeApiEndpointMethod.toLowerCase() === 'get') {
-      method = 'get';
-      endpoint += `/${startDate}/${scanRange}`;
-      args = [endpoint];
-    }
-
-    this._http
-      [method].apply(this._http, args)
-      .map((res: Response) => {
-        let jsonResult = res.json();
-
-        // throw error here so that we can handle it properly later
-        if (jsonResult.success === false) {
-          throw new Error('Map Data not found.');
-        }
-
-        return jsonResult;
-      })
-      .subscribe((response: any) => {
+    this._tileLayerService
+      .getNdviLayerData(startDate, scanRange)
+      .then((response: any) => {
         let tileUrl = this._tileLayerService.getEarthEngineMapUrl(response.mapId, response.mapToken);
 
-        this._layerId = response.mapId;
-
-        let payload: Layer = {
-          id: this._layerId,
+        // assemble the layer
+        let layer: Layer = {
+          id: response.mapId,
           url: tileUrl,
           type: 'ndvi',
           layerOptions: this._tileLayerService.getNdviLayerOptions()
         };
 
+        // set the layer id for the component instance
+        this._layerId = response.mapId;
+
         // clear tile layers before adding it
-        this._mapService
-          .clearTileLayers()
-          .then(() => {
-            // add the tile layer to the map
-            return this._mapService.addNewTileLayer(payload.id, payload.url, payload.layerOptions);
-          })
-          .catch((error: Error) => {
-            console.error(error);
-          })
-          ;
+        return Promise.all([
+          Promise.resolve(layer),
+          this._mapService.clearTileLayers()
+        ]);
+      })
+      .then((resolvedValue: any) => {
+        let layer: Layer = resolvedValue[0];
 
         // add the new layer to the store
         this._mapLayersStore.dispatch({
           type: 'ADD_LAYER',
-          payload: payload
+          payload: layer
         });
-      }, (error) => {
+
+        return this._mapService.addNewTileLayer(layer.id, layer.url, layer.layerOptions);
+      })
+      .catch((error) => {
         let message = error.message;
 
         if (typeof error.message === 'undefined') {
