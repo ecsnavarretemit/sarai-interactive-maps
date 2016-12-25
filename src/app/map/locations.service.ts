c5f99609da8a075fd117a0922822e0cdf36b62d9
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-12-25 15:35:51 +0800

    Add more tests for Locations Service

--- src/app/map/locations.service.ts
+++ src/app/map/locations.service.ts
@@ -6,7 +6,7 @@
  */
 
 import { Injectable, Inject } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { MAP_CONFIG } from './map.config';
 import 'rxjs/add/operator/map';
@@ -20,15 +20,27 @@ export class LocationsService {
   ) { }
 
   getRegions(): Observable<any> {
+    // assemble the request headers
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
     return this._http
-      .get(this._config.location_api.region.endpoint)
+      .get(this._config.location_api.region.endpoint, {
+        headers
+      })
       .map((res: Response) => res.json())
       ;
   }
 
   getProvinces(): Observable<any> {
+    // assemble the request headers
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
     return this._http
-      .get(this._config.location_api.province.endpoint)
+      .get(this._config.location_api.province.endpoint, {
+        headers
+      })
       .map((res: Response) => res.json())
       ;
   }
@@ -36,8 +48,14 @@ export class LocationsService {
   getProvincesByRegionId(regionId: number): Observable<any> {
     let endpoint = `${this._config.location_api.region.endpoint}${regionId}/provinces`;
 
+    // assemble the request headers
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
     return this._http
-      .get(endpoint)
+      .get(endpoint, {
+        headers
+      })
       .map((res: Response) => res.json())
       ;
   }
