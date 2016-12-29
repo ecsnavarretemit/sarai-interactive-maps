e2f0ac2f200decad16358eca08942e82c5e14f48
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-12-29 19:10:24 +0800

    Add mock class for locations service

---
+++ src/app/mocks/map/index.ts
@@ -0,0 +1,10 @@
+/*!
+ * Mock Map importer
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+export * from './locations.service.mock';
+
+
---
+++ src/app/mocks/map/locations.service.mock.ts
@@ -0,0 +1,81 @@
+/*!
+ * Locations Service (Mock)
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { Observable } from 'rxjs/Observable';
+import { LocationsService } from '../../map/locations.service';
+import 'rxjs/add/observable/of';
+export class MockLocationsService extends LocationsService {
+
+  constructor() {
+    super(null, null);
+  }
+
+  getRegions(): Observable<any> {
+    return Observable.of({
+      result: [
+        {
+          id: 100,
+          name: 'Ilocos Region',
+          region_normalized: 'Region 1',
+          region_normalized_canonical: 'Ilocos Region (Region 1)',
+          region_roman: 'Region I',
+          region_roman_canonical: 'Ilocos Region (Region I)',
+          slug: 'ilocos-region-region-i'
+        }
+      ],
+      success: true
+    });
+  }
+
+  getProvinces(): Observable<any> {
+    return Observable.of({
+      result: [
+        {
+          id: 100,
+          name: 'Ilocos Norte',
+          slug: 'ilocos-norte',
+        }
+      ],
+      success: true
+    });
+  }
+
+  getProvincesByRegionId(regionId: number): Observable<any> {
+    return Observable.of({
+      result: [
+        {
+          id: 100,
+          name: 'Ilocos Norte',
+          slug: 'ilocos-norte',
+        }
+      ],
+      success: true
+    });
+  }
+
+  getProvincesFromFT(): Observable<any> {
+    return Observable.of({
+      places: [
+        {
+          center: {
+          coordinates: [
+            120.679049423,
+            15.023545,
+            0.0
+          ],
+          type: 'Point'
+        },
+        name: 'Pampanga'
+        }
+      ],
+      success: true
+    });
+  }
+
+}
+
+
