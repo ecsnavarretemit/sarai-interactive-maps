b8cf7e90eda978984321181518626e4bfa304c47
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2016-12-30 16:46:30 +0800

    Extend BasePanelComponent (NdviPanelComponent)

--- src/app/map/ndvi-panel/ndvi-panel.component.ts
+++ src/app/map/ndvi-panel/ndvi-panel.component.ts
@@ -5,126 +5,19 @@
  * Licensed under MIT
  */
 
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { LeafletMapService } from '../../leaflet';
-import * as L from 'leaflet';
-import 'rxjs/add/operator/throttleTime';
-import 'rxjs/add/observable/fromEvent';
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  OnDestroy,
-  Output,
-  EventEmitter,
-  ViewChild,
-  ElementRef,
-  Renderer,
-  trigger,
-  state,
-  style,
-  transition,
-  animate
-} from '@angular/core';
+import { Component } from '@angular/core';
+import { BasePanelAnimation, BasePanelComponent } from '../base-panel/base-panel.component';
 
 @Component({
   selector: 'app-ndvi-panel',
   templateUrl: './ndvi-panel.component.html',
   styleUrls: ['./ndvi-panel.component.sass'],
   animations: [
-    trigger('controlWrapper', [
-      state('void', style({
-        height: 0
-      })),
-      state('visible', style({
-        opacity: 1,
-        height: 'auto'
-      })),
-      state('hidden', style({
-        opacity: 0,
-        height: 0
-      })),
-      transition('* => *', animate(500))
-    ])
+    BasePanelAnimation()
   ]
 })
-export class NdviPanelComponent implements OnInit, AfterViewInit, OnDestroy {
+export class NdviPanelComponent extends BasePanelComponent  {
   public controlWrapperAnimationState: string = 'hidden';
-  private _mouseOverSubscription: Subscription;
-  private _mouseLeaveListener: Function;
-
-  @Output() hideButtonClick: EventEmitter<Event> = new EventEmitter<Event>();
-  @ViewChild('controlwrapper') controlWrapper: ElementRef;
-
-  constructor(
-    private _renderer: Renderer,
-    private _mapService: LeafletMapService
-  ) { }
-
-  ngOnInit() { }
-
-  ngAfterViewInit() {
-    // since mouseover is fire continuously, we throttle it so that it is only fired every 600 ms
-    this._mouseOverSubscription = Observable
-      .fromEvent(this.controlWrapper.nativeElement, 'mouseover')
-      .throttleTime(600)
-      .subscribe(() => {
-        this.mouseMovementOnMapControl('over');
-      })
-      ;
-
-    // listen to the mouseleave event
-    this._mouseLeaveListener = this._renderer.listen(this.controlWrapper.nativeElement, 'mouseleave', () => {
-      this.mouseMovementOnMapControl('leave');
-    });
-  }
-
-  onHideButtonClick(event) {
-    // switch the panel animation state to hidden
-    this.controlWrapperAnimationState = 'hidden';
-
-    this.hideButtonClick.emit(event);
-  }
-
-  togglePanelVisibility() {
-    if (this.controlWrapperAnimationState === 'hidden') {
-      this.controlWrapperAnimationState = 'visible';
-      return;
-    }
-
-    this.controlWrapperAnimationState = 'hidden';
-  }
-
-  mouseMovementOnMapControl(type: string) {
-    this._mapService
-      .getMap()
-      .then((map: L.Map) => {
-        if (type === 'over') {
-          // disable dragging when the mouse is over the panel
-          map.dragging.disable();
-
-          // disable scroll wheel zoom when the mouse is over the panel
-          map.scrollWheelZoom.disable();
-        } else {
-          // enable dragging when the mouse is not ove the panel
-          map.dragging.enable();
-
-          // enable scroll wheel zoom when the mouse is not ove the panel
-          map.scrollWheelZoom.enable();
-        }
-      })
-      ;
-  }
-
-  ngOnDestroy() {
-    // remove event listener
-    this._mouseLeaveListener();
-
-    // remove the subscription from the event
-    this._mouseOverSubscription.unsubscribe();
-  }
-
 }
 
 
