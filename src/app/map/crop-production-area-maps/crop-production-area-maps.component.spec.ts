e9645e349be86282d514407c6f9711a7c82ea62f
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-09 14:41:16 +0800

    Add basic unit test for the crop production area map component

--- src/app/map/crop-production-area-maps/crop-production-area-maps.component.spec.ts
+++ src/app/map/crop-production-area-maps/crop-production-area-maps.component.spec.ts
@@ -10,16 +10,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { MapConfig, MAP_CONFIG } from '../map.config';
+import { TileLayerService } from '../tile-layer.service';
+import { LeafletMapService, LeafletModule } from '../../leaflet';
+import { MapLayersReducer, SuitabilityLevelsReducer } from '../../store';
+import { MockActivatedRoute, MockRouter } from '../../mocks/router';
 
 import { CropProductionAreaMapsComponent } from './crop-production-area-maps.component';
 
-describe('CropProductionAreaMapsComponent', () => {
+describe('Component: CropProductionAreaMaps', () => {
   let component: CropProductionAreaMapsComponent;
   let fixture: ComponentFixture<CropProductionAreaMapsComponent>;
+  let mockActivatedRoute: MockActivatedRoute;
+  let mockRouter: MockRouter;
 
   beforeEach(async(() => {
+    mockActivatedRoute = new MockActivatedRoute({
+      crop: 'rice'
+    });
+
+    mockRouter = new MockRouter();
+
     TestBed.configureTestingModule({
-      declarations: [ CropProductionAreaMapsComponent ]
+      declarations: [
+        CropProductionAreaMapsComponent
+      ],
+      imports: [
+        HttpModule,
+        LeafletModule
+      ],
+      providers: [
+        TileLayerService,
+        LeafletMapService,
+
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: MAP_CONFIG, useValue: MapConfig },
+
+        provideStore({
+          mapLayers: MapLayersReducer,
+          suitabilityLevels: SuitabilityLevelsReducer
+        }),
+      ]
     })
     .compileComponents();
   }));
@@ -30,7 +65,7 @@ describe('CropProductionAreaMapsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create an instance', () => {
     expect(component).toBeTruthy();
   });
 
