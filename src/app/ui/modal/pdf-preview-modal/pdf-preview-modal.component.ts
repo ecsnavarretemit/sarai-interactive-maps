4658f2e916955bf76a193dad5d6acc3ace1b985c
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-24 06:16:13 +0800

    Add PDF Preview modal

---
+++ src/app/ui/modal/pdf-preview-modal/pdf-preview-modal.component.ts
@@ -0,0 +1,64 @@
+/*!
+ * PDF Preview Modal Component
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { AfterViewInit, Component, Injector, Input, OnInit } from '@angular/core';
+import { BaseModalComponent } from '../base-modal/base-modal.component';
+
+@Component({
+  selector: 'app-pdf-preview-modal',
+  templateUrl: './pdf-preview-modal.component.html',
+  styleUrls: ['./pdf-preview-modal.component.sass']
+})
+export class PdfPreviewModalComponent extends BaseModalComponent implements OnInit, AfterViewInit {
+  public loaderVisible: boolean = true;
+  public modalVisible: boolean = false;
+  public hasPdf: boolean = false;
+
+  @Input('title') title: string = 'PDF Preview Title';
+  @Input('src') src: string;
+  @Input('downloadFilename') downloadFilename: string;
+
+  constructor(injector: Injector) {
+    // call the parent constructor
+    super(injector);
+
+    // retrieve properties from the injector
+    this.title = injector.get('title', 'PDF Preview Title');
+    this.src = injector.get('src', undefined);
+    this.downloadFilename = injector.get('downloadFilename', undefined);
+  }
+
+  ngOnInit() {
+    if (typeof this.src !== 'undefined') {
+      this.hasPdf = true;
+    }
+
+    // if no download filename has been provided, emulate basename behavior na provide it
+    if (typeof this.downloadFilename === 'undefined' && typeof this.src !== 'undefined') {
+      this.downloadFilename = this.src.split(/[\\/]/).pop();
+    }
+  }
+
+  onShow() {
+    this.modalVisible = true;
+  }
+
+  /**
+   * When calling this function as the after-load-complete callback of the pdf component,
+   * use: `[after-load-complete]="pdfLoadComplete.bind(this)"` instead of the this
+   * `[after-load-complete]="pdfLoadComplete"` because the pdf component changes the value of this
+   * to the value of this in the pdf component which makes us unable to get this component's `this` value.
+   */
+  onLoadComplete() {
+    // hide the loader indicator after 3s since the pdf viewer does not provide a callback
+    // after rendering the PDF
+    setTimeout(() => {
+      this.loaderVisible = false;
+    }, 3000);
+  }
+
+}
