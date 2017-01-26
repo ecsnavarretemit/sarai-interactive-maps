b0eb50c6b438003af8033d0502649e6eeac182b6
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-01-26 17:54:21 +0800

    Create modal that dynamically creates charts based on the component metadata

--- src/app/ui/modal/modal.module.ts
+++ src/app/ui/modal/modal.module.ts
@@ -10,11 +10,13 @@ import { CommonModule } from '@angular/common';
 import { ModalModule as BootstrapModalModule } from 'ng2-bootstrap/modal';
 import { PdfViewerComponent } from 'ng2-pdf-viewer';
 
+import { LineChartComponent } from '../charts';
 import { DynamicModalComponent } from './dynamic-modal/dynamic-modal.component';
 import { BaseModalComponent } from './base-modal/base-modal.component';
 import { SpawnModalService } from './spawn-modal.service';
 import { AlertModalComponent } from './alert-modal/alert-modal.component';
 import { PdfPreviewModalComponent } from './pdf-preview-modal/pdf-preview-modal.component';
+import { ChartModalComponent } from './chart-modal/chart-modal.component';
 
 @NgModule({
   imports: [
@@ -25,17 +27,22 @@ import { PdfPreviewModalComponent } from './pdf-preview-modal/pdf-preview-modal.
     DynamicModalComponent,
     BaseModalComponent,
     AlertModalComponent,
-    PdfPreviewModalComponent
+    PdfPreviewModalComponent,
+    ChartModalComponent
   ],
   declarations: [
     DynamicModalComponent,
     BaseModalComponent,
     AlertModalComponent,
     PdfPreviewModalComponent,
-    PdfViewerComponent
+    PdfViewerComponent,
+    ChartModalComponent
   ],
   providers: [
     SpawnModalService
+  ],
+  entryComponents: [
+    LineChartComponent
   ]
 })
 export class ModalModule { }
