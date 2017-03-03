317ae7242d2e106b01bb9cf79b6a80f43b3d00d4
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-03-03 21:28:34 +0800

    Create shared module to contain all services, interfaces and other code shared across the module

---
+++ src/app/map/shared/index.ts
@@ -0,0 +1,10 @@
+/*!
+ * Shared Exporter
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+export * from './shared.module';
+
+
---
+++ src/app/map/shared/shared.module.ts
@@ -0,0 +1,18 @@
+/*!
+ * Shared Module
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
+
+
