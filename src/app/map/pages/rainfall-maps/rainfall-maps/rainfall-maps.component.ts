470f3690d9e236951e01666c0306c93f7c2fad7a
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-03-26 12:16:28 +0800

    Implement CSV and Image export for rainfall data

--- src/app/map/pages/rainfall-maps/rainfall-maps/rainfall-maps.component.ts
+++ src/app/map/pages/rainfall-maps/rainfall-maps/rainfall-maps.component.ts
@@ -5,7 +5,7 @@
  * Licensed under MIT
  */
 
-import { Component, Inject, OnInit, OnDestroy, Renderer } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, OnDestroy, Renderer, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
@@ -17,6 +17,7 @@ import { LoggerService } from '../../../../shared';
 import { ChartModalComponent, LineChartComponent, SpawnModalService } from '../../../../ui';
 import { Layer } from '../../../../store';
 import { APP_CONFIG } from '../../../../app.config';
+import { MAP_CONFIG } from '../../../map.config';
 import * as Chart from 'chart.js';
 import * as moment from 'moment';
 import * as L from 'leaflet';
@@ -40,8 +41,11 @@ export class RainfallMapsComponent implements OnInit, OnDestroy {
   private _currentEndDate: string;
   private _oldCumulativeRainfallData: any;
 
+  @ViewChild('downloadFile') downloadFile: ElementRef;
+
   constructor(
     @Inject(APP_CONFIG) private _globalConfig: any,
+    @Inject(MAP_CONFIG) private _mapConfig: any,
     private _mapService: LeafletMapService,
     private _tileLayerService: TileLayerService,
     private _rainfallMapService: RainfallMapService,
@@ -89,6 +93,31 @@ export class RainfallMapsComponent implements OnInit, OnDestroy {
         // setup map click event listener
         this.setupMapClick(popupPane);
       });
+
+    // receive outputs from the dynamically create modal
+    this._modalService.outputStream
+      .debounceTime(300)
+      .subscribe((output: any) => {
+        switch (output.type) {
+          case 'image':
+            // download image
+            this._renderer.setElementProperty(this.downloadFile.nativeElement, 'href', output.data);
+            this._renderer.setElementProperty(this.downloadFile.nativeElement, 'download', 'chart.jpg');
+            this._renderer.setElementProperty(this.downloadFile.nativeElement, 'target', '_self');
+            this._renderer.invokeElementMethod(this.downloadFile.nativeElement, 'click');
+            break;
+
+          case 'csv':
+            const coords = this._marker.getLatLng();
+
+            this._renderer.setElementProperty(this.downloadFile.nativeElement, 'href', output.metadata.endpoint);
+            this._renderer.setElementProperty(this.downloadFile.nativeElement, 'target', '_blank');
+            this._renderer.invokeElementMethod(this.downloadFile.nativeElement, 'click');
+
+            break;
+        }
+      })
+      ;
   }
 
   setupMapClick(targetEl: HTMLElement) {
@@ -221,6 +250,10 @@ export class RainfallMapsComponent implements OnInit, OnDestroy {
     const parsedEndDate = moment(endDate, 'YYYY-MM-DD');
     let dataObservable: Observable<any>;
 
+    // assemble endpoint for download link
+    let endpoint = `${this._mapConfig.rainfall_maps.eeApiEndpoint}/cumulative-rainfall/`;
+    endpoint += `${coords.lat}/${coords.lng}/${startDate}/${endDate}?fmt=csv`;
+
     if (changed === false && typeof this._oldCumulativeRainfallData !== 'undefined') {
       dataObservable = Observable.of(this._oldCumulativeRainfallData);
     } else {
@@ -289,6 +322,9 @@ export class RainfallMapsComponent implements OnInit, OnDestroy {
           inputs: {
             title: `Cumulative Rainfall Data (${parsedStartDate.format('MMMM D, YYYY')} to ${parsedEndDate.format('MMMM D, YYYY')})`,
             openImmediately: true,
+            metadata: {
+              endpoint
+            },
             chartOptions: {
               type: LineChartComponent,
               inputs: {
