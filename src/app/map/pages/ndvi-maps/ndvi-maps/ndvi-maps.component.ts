55a5dc57693fd24ecaefd6aff01ccd50b47b9418
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-25 13:32:17 +0800

    Make chart points radius smaller and only visible on hover

--- src/app/map/pages/ndvi-maps/ndvi-maps/ndvi-maps.component.ts
+++ src/app/map/pages/ndvi-maps/ndvi-maps/ndvi-maps.component.ts
@@ -632,12 +632,12 @@ export class NdviMapsComponent implements OnDestroy, OnInit {
       borderJoinStyle: 'miter',
       pointBorderColor: 'rgba(75, 192, 192, 1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 2,
-      pointHoverRadius: 8,
+      pointBorderWidth: 1,
+      pointHoverRadius: 3,
       pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
       pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-      pointHoverBorderWidth: 3,
-      pointRadius: 5,
+      pointHoverBorderWidth: 1,
+      pointRadius: 0,
       pointHitRadius: 10,
       spanGaps: false
     }, dataset);
--- src/app/map/pages/rainfall-maps/rainfall-maps/rainfall-maps.component.ts
+++ src/app/map/pages/rainfall-maps/rainfall-maps/rainfall-maps.component.ts
@@ -437,12 +437,12 @@ export class RainfallMapsComponent implements OnInit, OnDestroy {
       borderJoinStyle: 'miter',
       pointBorderColor: 'rgba(75, 192, 192, 1)',
       pointBackgroundColor: '#fff',
-      pointBorderWidth: 2,
-      pointHoverRadius: 8,
+      pointBorderWidth: 1,
+      pointHoverRadius: 3,
       pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
       pointHoverBorderColor: 'rgba(220, 220, 220, 1)',
-      pointHoverBorderWidth: 3,
-      pointRadius: 5,
+      pointHoverBorderWidth: 1,
+      pointRadius: 0,
       pointHitRadius: 10,
       spanGaps: false
     }, dataset);
