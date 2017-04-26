ebad529af995bcc70e82180280ffc6706d4a2400
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-04-26 08:14:40 +0800

    Activate panel when navigating using the navigation bar

--- src/app/map/pages/home/map-type/map-type.component.ts
+++ src/app/map/pages/home/map-type/map-type.component.ts
@@ -5,25 +5,45 @@
  * Licensed under MIT
  */
 
-import { AfterViewInit, Component, ContentChild, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 import { BasePanelComponent } from '../../../ui';
 import { LeafletButtonComponent } from '../../../../leaflet/leaflet-button/leaflet-button.component';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-map-type',
   templateUrl: './map-type.component.html',
   styleUrls: ['./map-type.component.sass']
 })
-export class MapTypeComponent implements AfterViewInit {
+export class MapTypeComponent implements AfterViewInit, OnDestroy {
   public active = false;
+  private _routerEventSubscription: Subscription;
 
   @Input('activateImmediately') activateImmediately = false;
+  @Input('activateOnUrl') activateOnUrl: string;
   @Output() activate: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() deactivate: EventEmitter<boolean> = new EventEmitter<boolean>();
   @ContentChild('mapTypePanel') panel: BasePanelComponent;
   @ContentChild(LeafletButtonComponent) button: LeafletButtonComponent;
 
+  constructor(private _router: Router) { }
+
   ngAfterViewInit() {
+    this._routerEventSubscription = this._router.events
+      .filter((evt: any) => (evt instanceof NavigationEnd))
+      .subscribe((evt: NavigationEnd) => {
+        if (
+          typeof this.activateOnUrl !== 'undefined' &&
+          this._router.isActive(this.activateOnUrl, false) &&
+          this.active === false
+        ) {
+          this.toggleActiveState(true, true);
+        }
+      })
+      ;
+
     // activate the map type when activateImmediately is set to true
     if (this.activateImmediately === true) {
       this.toggleActiveState(true, true);
@@ -52,6 +72,13 @@ export class MapTypeComponent implements AfterViewInit {
       this.activate.emit(this.active);
     } else {
       this.deactivate.emit(this.active);
+    }
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to the router.events
+    if (typeof this._routerEventSubscription !== 'undefined') {
+      this._routerEventSubscription.unsubscribe();
     }
   }
 
--- src/app/mocks/router/router.mock.ts
+++ src/app/mocks/router/router.mock.ts
@@ -5,10 +5,19 @@
  * Licensed under MIT
  */
 
+import { Observable } from 'rxjs/Observable';
+
 // mock classes for router integration testing
 // reference: <https://dzone.com/articles/getting-started-and-testing-with-angular-cli-and-angular-2-rc5-part-2>
 export class MockRouter {
   navigate = jasmine.createSpy('navigate');
+
+  get events(): Observable<any> {
+    return Observable.of({
+      id: 1,
+      url: '/test-url'
+    });
+  }
 }
 
 
